6cd9fef2ee8e80ebc98aad8b4d379f45276db932
Vitor Ivens <vitorivens@gmail.com>
2026-08-09 22:53:21 -0300

    add offline service worker

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='my-performance-v1.0.0';
+const ASSETS=['./','./index.html','./styles.css','./data-core.js','./data-gsa1.js','./data-gsa2.js','./app-core.js','./app-render.js','./app-ui.js','./cloud-sync.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET'||new URL(e.request.url).pathname.startsWith('/api/'))return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))
+});
