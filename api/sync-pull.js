1a04004cc097e822d24b041567ad1fe9fc797599
Vitor Ivens <vitorivens@gmail.com>
2026-08-12 16:55:01 -0300

    refactor(vercel): import server-only Neon helper

--- api/sync-pull.js
+++ api/sync-pull.js
@@ -1,2 +1,2 @@
-import {neonRpc} from './_neon-data-api.js';
+import {neonRpc} from '../server/neon-data-api.js';
 export default async function handler(request,response){return neonRpc(request,response,'my_performance_pull',{});}
